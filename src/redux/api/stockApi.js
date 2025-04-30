098b1410e098cd3b77b719af03eafeb8388bb43a
mygitaza <mongss122@hotmail.com>
2025-04-30 18:01:26 +0100

    fix stock

--- src/redux/api/stockApi.js
+++ src/redux/api/stockApi.js
@@ -3,33 +3,33 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const stockApi = createApi({
     reducerPath: 'stockApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://stockbackend-znbu.onrender.com',
+        baseUrl: 'https://stockbackend-znbu.onrender.com/api/stock',
         credentials: 'include',
     }),
     tagTypes: ['stock'],
     endpoints: (builder)=> ({
         addStock: builder.mutation({
             query: ({userId, units}) => ({
-                url: '/api/stock/add',
+                url: '/add',
                 method: 'POST',
                 body: { userId, units},
             }),
             invalidatesTags: ['stock'],
         }),
         getUserStocks: builder.query({
-            query: (userId) => `/api/stock/user/${userId}`,
+            query: (userId) => `/user/${userId}`,
             providesTags: ['stock'],
     }),
     approveStock: builder.mutation({
         query: ({userId, stockId})=>({
-            url: `/api/stock/approve/${userId}/${stockId}`,
+            url: `/approve/${userId}/${stockId}`,
             method: 'PUT',
         }),
         invalidatesTags: ['stock'],
     }),
     deleteStock: builder.mutation({
         query: ({ userId, stockId }) => ({
-            url: `/api/stock/delete/${userId}/${stockId}`,
+            url: `/delete/${userId}/${stockId}`,
             method: 'DELETE',
         }),
         invalidatesTags: ['stock'],
