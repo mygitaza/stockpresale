ec86c52c6cb6aa2b83d2dc6f1de18419aebe4633
mygitaza <mongss122@hotmail.com>
2025-04-30 13:01:39 +0100

    updated chart

--- src/pages/Invest/Invest.jsx
+++ src/pages/Invest/Invest.jsx
@@ -1,26 +1,54 @@
 import React from 'react'
 import './Invest.css'
 import { Link, useNavigate, useOutletContext } from 'react-router-dom'
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { useSelector } from 'react-redux'
 
 
-const invest_list = [
+const investData = [
     {
-        title: "Invest 1000+ for",
-        subtitle: "5% Bonus"
+        name: 'May',
+        stock: "MODE Inc",
+        unit: 0.9,
+        ModeInc: 120,
+        stockIndex: 129
     },
     {
-        title: "Invest 5000+ for",
-        subtitle: "10% Bonus"
+        name: 'Jun',
+        stock: "MODE Inc",
+        unit: 12,
+        ModeInc: 250,
+        stockIndex: 280
     },
     {
-        title: "Invest 10000+ for",
-        subtitle: "15% Bonus"
+        name: 'Aug',
+        stock: "MODE Inc",
+        unit: 14,
+        ModeInc: 312,
+        stockIndex: 390
     },
     {
-        title: "Invest 20000+ for",
-        subtitle: "20% Bonus"
+        name: 'Sept',
+        stock: "MODE Inc",
+        unit: 16,
+        ModeInc: 456,
+        stockIndex: 500
     },
+    {
+        name: 'Dec',
+        stock: "MODE Inc",
+        unit: 18,
+        ModeInc: 710,
+        stockIndex: 800
+    },
+    {
+        name: 'Dec',
+        stock: "MODE Inc",
+        unit: 18,
+        ModeInc: 1000,
+        stockIndex: 1250
+    },
+    
 ]
 const Invest = () => {
     const {setShowLogin} = useOutletContext();
@@ -42,14 +70,29 @@ const Invest = () => {
         <h1 className='invest-headerTwo'>20% Bonus Shares</h1>
         </div>
         <div className="invest-wrapper">
-            {
-                invest_list.map((item,index)=>(
-                    <div className='invest-content' key={index}>
-                        <h1>{item.title}</h1>
-                        <h1 className='invest-content-headerOne'>{item.subtitle}</h1>
-                    </div>
-                ))
-            }
+        <ResponsiveContainer width="100%" height="100%">
+        <AreaChart width={500} height={400} data={investData} margin={{right: 30}}>
+        <YAxis/>
+        <XAxis dataKey="name"/>
+        <CartesianGrid strokeDasharray="5 5"/>
+        <Tooltip/>
+        <Legend/>
+            <Area 
+            type="monotone"
+            stroke='#2563eb'
+            fill='#3b82f6'
+            dataKey="ModeInc" 
+            stackId="1"
+            />
+            <Area 
+            type="monotone"
+            stroke='#7c3aed'
+            fill='#8b5cf6'
+            dataKey="stockIndex" 
+            stackId="1"
+            />
+        </AreaChart>
+        </ResponsiveContainer>
         </div>
         <button onClick={handlePreSaleClick} className='presale-btn'>Join the Presale</button>
     </div>
