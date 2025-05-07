8c2c5638b0532a1c920b37b6beafab70c3acad39
mygitaza <mongss122@hotmail.com>
2025-05-07 22:33:12 +0100

    fixed livechat

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import React, { useState } from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
+import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
+  useEffect(() => {
+    const tawk = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0];
+    tawk.async = true;
+    tawk.src = "https://embed.tawk.to/681bc9a6c905ab190eadbb7c/1iqm8ndo9";
+    tawk.charset = "UTF-8";
+    tawk.setAttribute("crossorigin", "*");
+    document.body.appendChild(tawk);
+
+    return () => {
+      document.body.removeChild(tawk);
+    };
+  }, []);
   return (
     <>
-    <div className="app">
-      <Navbar />
-      <Outlet />
-      <Footer/>
-    </div>
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
